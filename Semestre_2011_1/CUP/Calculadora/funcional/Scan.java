e633939455896556adcf1a14f898c2c3cbf50527
unknown <Xiul@.(none)>
2011-08-01 10:37:21 -0430

    Añadidos ejercicios de la calculadora (solo sintaxis y evaluando expresiones) en CUP sin JFlex

---
+++ Semestre_2011_1/CUP/Calculadora/funcional/Scan.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java_cup.runtime.*;
+
+
+class Scan implements java_cup.runtime.Scanner {
+
+	private java.io.BufferedReader FileIn;
+	
+	Scan(java.io.InputStream instream){
+		FileIn = new java.io.BufferedReader(
+			new java.io.InputStreamReader(instream)
+		);
+	}
+	
+	public java_cup.runtime.Symbol next_token()
+	throws java.io.IOException
+	{
+		int c=0;
+		do{
+			try
+			{
+				c=FileIn.read();
+			}catch(IOException e){
+				System.out.println("Error");
+			}
+			
+			switch(c){
+			case '1':
+			{return new java_cup.runtime.Symbol(sym.DIGITO,new Integer(1));}
+			case '2':
+			{return new java_cup.runtime.Symbol(sym.DIGITO,new Integer(2));}				
+			case '3': 
+			{return new java_cup.runtime.Symbol(sym.DIGITO,new Integer(3));}
+			case '4':
+			{return new java_cup.runtime.Symbol(sym.DIGITO,new Integer(4));}
+			case '5': 
+			{return new java_cup.runtime.Symbol(sym.DIGITO,new Integer(5));}
+			case '6': 
+			{return new java_cup.runtime.Symbol(sym.DIGITO,new Integer(6));}
+			case '7':
+			{return new java_cup.runtime.Symbol(sym.DIGITO,new Integer(7));}				
+			case '8':
+			{return new java_cup.runtime.Symbol(sym.DIGITO,new Integer(8));}				
+			case '9':
+			{return new java_cup.runtime.Symbol(sym.DIGITO,new Integer(9));}				
+			case '0':
+			{return new java_cup.runtime.Symbol(sym.DIGITO,new Integer(0));}				
+
+			case '+': {return new java_cup.runtime.Symbol(sym.SUMA);}
+			case '-': {return new java_cup.runtime.Symbol(sym.RESTA);}
+			case '*': {return new java_cup.runtime.Symbol(sym.MULTIPLICACION);}
+			case '/': {return new java_cup.runtime.Symbol(sym.DIVISION);}
+			case '(': {return new java_cup.runtime.Symbol(sym.LPARENT);}
+			case ')': {return new java_cup.runtime.Symbol(sym.RPARENT);}
+			case '^': {return new java_cup.runtime.Symbol(sym.POTENCIA);}
+			case '=': {return new java_cup.runtime.Symbol(sym.RESULTADO);}
+			default: {break;}
+			}
+			}while(true);
+		}
+		
+	}
